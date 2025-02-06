ee394d18eefbcc446486494ac471ef77bef49346
Balajiganesh21 <145132060+Balajiganesh21@users.noreply.github.com>
2025-02-06 14:38:12 +0530

    Update coffie.java

--- coffie.java
+++ coffie.java
@@ -3,7 +3,7 @@ package day1;
 
 public class coffie {
 	public static void main(String[] args) {
-		System.out.println("Thank for your order")
+		System.out.println("Thank for your order");
 	
     }
 }
