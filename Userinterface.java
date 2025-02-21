72d70206e92c4de515f86d5deec1da83049a7fea
Balajiganesh21 <145132060+Balajiganesh21@users.noreply.github.com>
2025-02-21 16:44:31 +0530

    simpleEmpolyee management

---
+++ Userinterface.java
@@ -0,0 +1,36 @@
+package employeeMangement;
+
+public class Userinterface {
+	
+	public static void main(String[] args) {
+		ex2 user = new ex2();
+		
+		user.name = "balaji";
+		user.id = 1;
+		user.phoneNo = 1234567;
+		user.display();
+		
+		System.out.println();
+		
+		user.name = "sanjiv";
+		user.id = 2;
+		user.phoneNo = 234587;
+		user.display();
+		
+		System.out.println();
+		
+		user.name = "loki";
+		user.id = 3;
+		user.phoneNo = 1345677;
+		user.display();
+		
+		System.out.println();
+		
+		user.name = "ganesh";
+		user.id = 4;
+		user.phoneNo = 1563217;
+		user.display();
+		
+	}
+
+}
---
+++ ex2.java
@@ -0,0 +1,16 @@
+package employeeMangement;
+
+public class ex2 {
+	
+	String name;
+	int id;
+	long phoneNo;
+	
+	
+	
+	void display() {
+	System.out.println("Enter name : " + name);
+	System.out.println("Enter id : " + id);
+	System.out.println("Enter phoneNo : " + phoneNo);
+	}
+}
